2579e7bbfa83cc2ea5d1d617ff4839b59cb476c5
Shahirul <shahiruliimalo@gmail.com>
2025-01-25 03:11:40 +0800

    feat:🦄 appwrite setup, client setup, and override react dependencies

---
+++ lib/appwrite/config.ts
@@ -0,0 +1,12 @@
+export const appwriteConfig = {
+  // ! so that typescript will know that it exists
+  // we use PUBLIC when the env will be publicly available at the front end,
+  // if it is only for the backend server side action then we dont use PUBLIC (in this case the admin actions will be on the server side only)
+  endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
+  projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
+  databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
+  usersCollectionId: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION!,
+  filesCollectionId: process.env.NEXT_PUBLIC_APPWRITE_FILES_COLLECTION!,
+  bucketId: process.env.NEXT_PUBLIC_APPWRITE_BUCKET!,
+  secretKey: process.env.NEXT_APPWRITE_SECRET!,
+};
