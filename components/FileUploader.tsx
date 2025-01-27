a78ca75d558b187cf51f88c69f01a4d02fbb7d89
Shahirul <shahiruliimalo@gmail.com>
2025-01-27 18:58:49 +0800

    feat:🦄 begin implementing file upload and thumbnail for each file type

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,7 +1,103 @@
-import React from "react";
+"use client";
 
-const FileUploader = () => {
-  return <div>FileUploader</div>;
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { Button } from "./ui/button";
+import { cn, convertFileToUrl, getFileType } from "@/lib/utils";
+import Image from "next/image";
+import Thumbnail from "./Thumbnail";
+
+interface Props {
+  ownerId: string;
+  accountId: string;
+  className?: string;
+}
+const FileUploader = ({ ownerId, accountId, className }: Props) => {
+  const [files, setFiles] = useState<File[]>([]); // type of file array
+
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
+    setFiles(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  // handles the removal of a specific file from the upload list
+  const handleRemoveFile = (
+    // specifying eventhough the remove button is an image, it is also a mouse event
+    e: React.MouseEvent<HTMLImageElement, MouseEvent>,
+    fileName: string
+  ) => {
+    // stops the event from bubbling up to parent elements, preventing unintended behavior when clicking on the file removal icon
+    e.stopPropagation();
+    // filters out the file to be removed while keeping the rest in the list
+    setFiles((prevFiles) => prevFiles.filter((file) => file.name !== fileName));
+  };
+
+  return (
+    <div {...getRootProps()} className="cursor-pointer">
+      <input {...getInputProps()} />
+      <Button type="button" className={cn("uploader-button", className)}>
+        <Image
+          src="/assets/icons/upload.svg"
+          alt="upload"
+          width={24}
+          height={24}
+        />
+        <p>Upload</p>
+      </Button>
+
+      {/* bulk upload so that we know the progress for each file that is being uploaded*/}
+      {files.length > 0 && (
+        <ul className="uploader-preview-list">
+          <h4 className="h4 text-light-100">Uploading</h4>
+          {files.map((file, index) => {
+            const { type, extension } = getFileType(file.name); // to know the type of each uploaded file
+
+            return (
+              // do the key like this to make it really unique
+              <li
+                key={`${file.name}-${index}`}
+                className="uploader-preview-item"
+              >
+                <div className="flex items-center gap-3">
+                  {/* thumbnail to have a preview of the file that we are trying to upload */}
+                  <Thumbnail
+                    type={type}
+                    extension={extension}
+                    url={convertFileToUrl(file)}
+                  />
+                  <div className="preview-item-name">
+                    {file.name}
+
+                    <Image
+                      src="/assets/icons/file-loader.gif"
+                      width={80}
+                      height={26}
+                      alt="Loader"
+                    />
+                  </div>
+                </div>
+                {/* cancel or remove upload for a specific file */}
+                <Image
+                  src="/assets/icons/remove.svg"
+                  alt="Remove"
+                  width={24}
+                  height={24}
+                  onClick={(e) => handleRemoveFile(e, file.name)} // pass the event and the file name
+                />
+              </li>
+            );
+          })}
+        </ul>
+      )}
+
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
 };
 
 export default FileUploader;
