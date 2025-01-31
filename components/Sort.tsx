a0238548c124117082bd6b4185a20851aef4a8f8
Shahirul <shahiruliimalo@gmail.com>
2025-01-31 12:03:17 +0800

    feat:🦄 add sorting files functionality

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,7 +1,41 @@
+"use client";
 import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useRouter, usePathname } from "next/navigation";
+import { sortTypes } from "@/constants";
 
 const Sort = () => {
-  return <div>Sort</div>;
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleSort = (value: string) => {
+    router.push(`${pathname}?sort=${value}`); // push the new sort value to the url which will then be handled by the getFiles function
+  };
+
+  return (
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((sort) => (
+          <SelectItem
+            key={sort.value}
+            value={sort.value}
+            className="shad-select-item"
+          >
+            {sort.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
 };
 
 export default Sort;
